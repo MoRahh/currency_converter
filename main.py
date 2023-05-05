62c1484240ae82b9eb90824dfb6301b3d90cc0ec
MoRahh <moodyaburahma@gmail.com>
2023-05-05 19:43:55 +0300

    Implemented Breadth First Search Algorithm and printed the shortest path

--- main.py
+++ main.py
@@ -15,13 +15,17 @@ maze = [
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
 
+
 def print_maze(maze, stdscr, path=[]):
     BLUE = curses.color_pair(1)
     RED = curses.color_pair(2)
 
-    for i,row in enumerate(maze):
-        for j,value in enumerate(row):
-            stdscr.addstr(i, j*2, value, BLUE)
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if (i, j) in path:
+                stdscr.addstr(i, j*2, "X", RED)
+            else:
+                stdscr.addstr(i, j*2, value, BLUE)
 
 def find_start(maze, start):
     for i,row in enumerate(maze):
@@ -46,8 +50,24 @@ def find_path(maze, stdscr):
         current_pos, path = q.get()
         row,col = current_pos
 
+        stdscr.clear()
+        print_maze(maze, stdscr, path)
+        stdscr.refresh()
+
         if maze[row][col] == end:
             return path
+        
+        neighbors = find_neighbors(maze,row,col)
+        for neighbor in neighbors:
+            if neighbor in visited:
+                continue
+
+            r, c = neighbor
+            if maze[r][c] == "#":
+                continue
+            new_path = path + [neighbor]
+            q.put((neighbor, new_path))
+            visited.add(neighbor)
         
 
 def find_neighbors(maze, row, col):
@@ -72,9 +92,7 @@ def main(stdscr):
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
 
 
-    stdscr.clear()
-    print_maze(maze, stdscr)
-    stdscr.refresh()
+    find_path(maze, stdscr)
     stdscr.getch()
 
 
